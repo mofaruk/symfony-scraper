fe6792feaeb6cf0fd6c06d7c99d77b1922118fa9
Md Omar Faruk <omar.faruk.ruet@gmail.com>
2023-08-01 23:44:48 +0600

    configure mysql 8 using docker compose

--- app/index.php
+++ app/index.php
@@ -1,3 +1,14 @@
 <?php
+$servername = "mysql";
+$username = "root";
+$password = "password";
 
-echo "Hello World";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=mysql", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+?>
